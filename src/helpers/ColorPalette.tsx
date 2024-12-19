f338d258e24ce78989c41b63d04c8d1e32097df1
ahammer0 <axel.schwindenhammer@gmail.com>
2024-12-19 16:40:06 +0100

    add css for buttons

--- src/helpers/ColorPalette.tsx
+++ src/helpers/ColorPalette.tsx
@@ -8,8 +8,8 @@ const ColorPalette = () => {
         <div className="testDiv bg-secondary">secondary</div>
         <div className="testDiv bg-danger">danger</div>
         <div className="testDiv bg-light">light</div>
-        <div className="testDiv bg-dark">dark</div>
-        <div className="testDiv bg-bgDark">bgDark</div>
+        <div className="testDiv bg-dark text-textLight">dark</div>
+        <div className="testDiv bg-bgDark text-textLight">bgDark</div>
         <div className="testDiv bg-bgLight">bgLight</div>
       </div>
       <div>
@@ -21,10 +21,24 @@ const ColorPalette = () => {
         <p className="text-textDark">textDark</p>
         <p className="text-textLight bg-bgDark">textLight</p>
       </div>
-      <h1>titre H1</h1>
-      <h2>titre H2</h2>
-      <h3>titre H3</h3>
-      <h4>titre H4</h4>
+      <div>
+        <h1>titre H1</h1>
+        <h2>titre H2</h2>
+        <h3>titre H3</h3>
+        <h4>titre H4</h4>
+        <p className="font-poppins">font poppins</p>
+        <p className="font-bungee">font bungee</p>
+        <div className="btn btn-primary">btn-primary</div>
+        <div className="btn btn-secondary">btn-secondary</div>
+        <div className="btn btn-danger">btn-danger</div>
+        <div className="btn btn-primary btn-outline">btn-primary outline</div>
+        <div className="btn btn-secondary btn-outline">btn-secondary outline</div>
+        <div className="btn btn-danger btn-outline">btn-danger outline</div>
+        <div className="btn btn-primary btn-cta">btn-primary cta</div>
+        <div className="btn btn-secondary btn-cta">btn-secondary cta</div>
+      </div>
+
+
     </div>
   );
 };
