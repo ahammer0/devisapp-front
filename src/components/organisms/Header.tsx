70fc7f8a320365b4f8f535d0cac591cc095d0af4
ahammer0 <axel.schwindenhammer@gmail.com>
2024-12-19 21:45:18 +0100

    add fa icons

--- src/components/organisms/Header.tsx
+++ src/components/organisms/Header.tsx
@@ -1,14 +1,25 @@
-import "./organisms.scss";
 import { useState } from "react";
+import { Link } from "react-router-dom";
+
+import "./organisms.scss";
 import MenuBurgerIcon from "../atoms/MenuBurgerIcon";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faRightToBracket,faHouse} from "@fortawesome/free-solid-svg-icons";
 
 const Header = () => {
   const [isActive, setIsActive] = useState(false);
   return (
     <header>
-      <MenuBurgerIcon isActive={isActive} toggle={()=>setIsActive(!isActive)} />
-      <p>header</p>
-      <p>header</p>
+      <nav>
+        <MenuBurgerIcon isActive={isActive} toggle={()=>setIsActive(!isActive)} />
+        <p>header</p>
+        <div>
+          <Link to="/"><FontAwesomeIcon icon={faHouse} className="icon"/></Link>
+          <Link to="/login">
+            <FontAwesomeIcon icon={faRightToBracket} className="icon"/>
+          </Link>
+        </div>
+      </nav>
     </header>
   );
 };
