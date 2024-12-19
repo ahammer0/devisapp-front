d7b1a0c2645bc013f2a8611ef20f5b139c27ac87
ahammer0 <axel.schwindenhammer@gmail.com>
2024-12-19 17:29:27 +0100

    add tables& input style

--- src/helpers/ColorPalette.tsx
+++ src/helpers/ColorPalette.tsx
@@ -32,13 +32,64 @@ const ColorPalette = () => {
         <div className="btn btn-secondary">btn-secondary</div>
         <div className="btn btn-danger">btn-danger</div>
         <div className="btn btn-primary btn-outline">btn-primary outline</div>
-        <div className="btn btn-secondary btn-outline">btn-secondary outline</div>
+        <div className="btn btn-secondary btn-outline">
+          btn-secondary outline
+        </div>
         <div className="btn btn-danger btn-outline">btn-danger outline</div>
         <div className="btn btn-primary btn-cta">btn-primary cta</div>
         <div className="btn btn-secondary btn-cta">btn-secondary cta</div>
       </div>
-
-
+      <form action="">
+        <label htmlFor="monChamp">label du champ</label>
+        <input type="text" name="monChamp" id="monChamp"/>
+        <button type="submit" className="btn btn-primary">
+          submit
+        </button>
+      </form>
+      <div className="flex flex-row" style={{justifyContent:"center"}}>
+        <div className="card" style={{width: "300px"}}>
+        Ipsum animi ipsum esse corrupti voluptas? Voluptate a ossimus sint nemo rerum
+        </div>
+        <div className="card" style={{width: "300px"}}>
+        Ipsum animi ipsum esse corrupti voluptas? Voluptate a ossimus sint nemo rerum
+        </div>
+      </div>
+      <table>
+        <thead>
+          <tr>
+            <th>#id</th>
+            <th>titrre</th>
+            <th>nomtitrree</th>
+            <th>nomtitrre</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>1</td>
+            <td>tatat</td>
+            <td>naursituiui</td>
+            <td>ndtluirst</td>
+          </tr>
+          <tr>
+            <td>2</td>
+            <td>tatat</td>
+            <td>naursituiui</td>
+            <td>ndtluirst</td>
+          </tr>
+          <tr>
+            <td>3</td>
+            <td>tatat</td>
+            <td>naursituiui</td>
+            <td>ndtluirst</td>
+          </tr>
+          <tr>
+            <td>3</td>
+            <td>tatat</td>
+            <td>naursituiui</td>
+            <td>ndtluirst</td>
+          </tr>
+        </tbody>
+      </table>
     </div>
   );
 };
