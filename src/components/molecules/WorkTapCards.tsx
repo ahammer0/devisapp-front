d9871c8fdbcef0fac7d94587166ee766e055c4e7
ahammer0 <axel.schwindenhammer@gmail.com>
2025-02-11 18:30:27 +0100

    edit:widening worktapcards

--- src/components/molecules/WorkTapCards.tsx
+++ src/components/molecules/WorkTapCards.tsx
@@ -1,22 +1,29 @@
 import useWorks from "../../hooks/useWorks";
 import { work } from "../../types/works";
+import "./WorkTapCards.scss";
 const WorkTapCards = ({ handleTap }: { handleTap: (work: work) => void }) => {
   const works = useWorks();
 
   return (
-    <div>
-      <h2>Ajouter des éléments</h2>
+    <div className="workTapCards">
+      <div>
+        {" "}
+        <h2>Ajouter des éléments</h2>
+      </div>
       {works.error && <p className="error">{works.error}</p>}
       {works.isLoading && <p>Loading...</p>}
-      {works.works.map((work) => (
-        <button
-          className="btn btn-secondary btn-outline"
-          key={work.id}
-          onClick={() => handleTap(work)}
-        >
-          {work.name}
-        </button>
-      ))}
+      <div className="cardGroup">
+        {works.works.map((work) => (
+          <button
+            type="button"
+            className="btn btn-secondary btn-outline tapCard"
+            key={work.id}
+            onClick={() => handleTap(work)}
+          >
+            {work.name}
+          </button>
+        ))}
+      </div>
     </div>
   );
 };
